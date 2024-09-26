1882cb7300cb2707dfcf88b75ede0094ee519004
ExploitDeveloperDil <166608060+ExploitDeveloperDil@users.noreply.github.com>
2024-09-26 10:22:03 +0400

    Update backdoor.c

--- backdoor.c
+++ backdoor.c
@@ -1,16 +1,17 @@
 // Loading necessary libaries for backdoor
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdio.h> //Allows to do basic input output functions
+#include <stdlib.h> //Allows to do basic general purpose functions like malloc
+#include <unistd.h> //Allows to do UNIX OS functions like file operations and process control.
+#include <string.h> //Allows string manipulation functions (text)
+#include <sys/stat.h> //Allows working with file information and process control
+#include <sys/types.h> //Allows working with file information and process control
 
 //Loading windows libaries
-#include <winsock2.h> //used to windows socket program.
-#include <windows.h>
-#include <winuser.h>
-#include <wininet.h>
-#include <windowsx.h>
+#include <winsock2.h> //Allows program to communicate over networks.
+#include <windows.h> //Windows API header, provide access to machine core functions
+#include <winuser.h> //Allows to create user interface in windows applications.
+#include <wininet.h> //Allow program to do internet options like downloading and uploading.
+#include <windowsx.h> //Allows to do extra macro functions to make windows program easier.
 
 
+int main();
