9443ec00ece9dfe057e4a3091f2bc87fc6b6e926
Vasan Dilaksan <vasandilaksan@gmail.com>
2024-10-19 22:53:17 +0400

    Shellcode program Started

---
+++ CS/shellcode.c
@@ -0,0 +1,16 @@
+//Executing the Shellcode using windows API.
+//Let's load the wanted libraries.
+#include <windows.h> //header for windows operations
+#include <stdio.h>
+
+//Entry Point
+int main()
+{
+    //Now create wanted variables.
+    char shellcode[] = {}; //To specify the generated shell code.
+    int shellcode_size = sizeof(shellcode); //To specify the size of the shellcode.
+
+    
+
+
+}
